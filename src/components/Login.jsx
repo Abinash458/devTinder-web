e0237323b9070fd3683c66aba2f19bbea9c1af50
abinash458 <amohapatra458@gmail.com>
2025-10-05 19:45:36 +0530

    install redux-toolkit and configure store also refactore code files

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
@@ -17,9 +23,8 @@ const Login = () => {
           withCredentials: true,
         }
       );
-
-      const data = await res.data;
-      console.log(data);
+      dispatch(addUser(res.data.data));
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
