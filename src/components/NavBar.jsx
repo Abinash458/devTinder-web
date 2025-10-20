4951c94a9961aaddb32f949255797c67c87e0431
abinash458 <amohapatra458@gmail.com>
2025-10-20 14:50:09 +0530

    complet razorpay integration and done the first phase to create the order and payment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,16 +46,19 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow-lg"
             >
               <li>
-                <Link to="/profile" className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </Link>
+                <Link to="/profile">Profile</Link>
               </li>
               <li>
                 <Link to="/connections">Connections</Link>
               </li>
               <li>
                 <Link to="/requests">Requests</Link>
+              </li>
+              <li>
+                <Link to="/premium" className="justify-between">
+                  Premium
+                  <span className="badge">New</span>
+                </Link>
               </li>
               <li>
                 <a onClick={handleLogout}>Logout</a>
---
+++ src/components/Premium.jsx
@@ -0,0 +1,216 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
+const Premium = () => {
+  const handleBuyCLick = async (membershipType) => {
+    try {
+      const order = await axios.post(
+        BASE_URL + "/payment/create",
+        { membershipType },
+        { withCredentials: true }
+      );
+
+      const { amount, keyId, currency, notes, orderId } = order.data;
+
+      const options = {
+        key: keyId,
+        amount,
+        currency: currency,
+        name: "DevTinder",
+        description: "Connect to other developers",
+        order_id: orderId,
+        prefill: {
+          name: notes.firstName + " " + notes.lastName,
+          email: notes.emailId,
+        },
+        theme: {
+          color: "#F37254",
+        },
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row justify-center gap-3 my-30">
+      <div className="card w-96 bg-base-300 shadow-lg">
+        <div className="card-body">
+          <div className="flex justify-between">
+            <h2 className="text-3xl font-bold">Silver</h2>
+            <span className="text-xl">₹300/mo</span>
+          </div>
+          <ul className="mt-6 flex flex-col gap-2 text-xs">
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Chat with other people</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>100 connection Request per day</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Blue Tick</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>3 months</span>
+            </li>
+          </ul>
+          <div className="mt-6">
+            <button
+              onClick={() => handleBuyCLick("silver")}
+              className="btn btn-primary btn-block"
+            >
+              Buy Silver
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-300 shadow-lg">
+        <div className="card-body">
+          <div className="flex justify-between">
+            <h2 className="text-3xl font-bold">Gold</h2>
+            <span className="text-xl">₹700/mo</span>
+          </div>
+          <ul className="mt-6 flex flex-col gap-2 text-xs">
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Chat with other people</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>100 connection Request per day</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Blue Tick</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>6 months</span>
+            </li>
+          </ul>
+          <div className="mt-6">
+            <button
+              onClick={() => handleBuyCLick("gold")}
+              className="btn btn-primary btn-block"
+            >
+              Buy Gold
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Premium;
